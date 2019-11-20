8ac378a2ad10f05624a250446bdf80b263bcea15
al159799BrandonGarcia <al159799@alumnos.uacj.mx>
2019-11-19 22:01:52 -0700

    Cree la sección de bitácora

---
+++ src/app/bitacora/bitacoras.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bitacoras',
+  templateUrl: './bitacoras.page.html',
+  styleUrls: ['./bitacoras.page.scss'],
+})
+export class BitacorasPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
