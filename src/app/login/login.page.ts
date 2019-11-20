fb35c9b6ce1af6e09d0875e7649e272fd5523578
Armin-AH <armin.alejandro.ah@gmail.com>
2019-11-19 20:08:06 -0700

    Agregué un botón para pasar de la home page al login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  title = "Hola mundo";
+  email = "";
 
   constructor() { }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.title = "Adios mundo :'("
+    }, 5000);
+
+  }
+
+  login() {
+    console.log(this.email);
   }
 
 }
