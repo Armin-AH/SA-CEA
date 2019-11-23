fff4de7dc078b1e38c4748e5ef6be7c8aba8e1e2
Armin-AH <armin.alejandro.ah@gmail.com>
2019-11-23 00:52:45 -0700

    Se agregó altas de citas

--- src/app/cita/cita.page.ts
+++ src/app/cita/cita.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BDService } from '../services/bd.service';
 
 @Component({
   selector: 'app-cita',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cita.page.scss'],
 })
 export class CitaPage implements OnInit {
-
-  constructor() { }
+  hoy; notas;carrera;motivo;fecha;nombre;id="";
+  listadoCita; registrar:Boolean=false
+  constructor(private BD:BDService) { }
 
   ngOnInit() {
+    console.log(this.listadoCita)
+    this.listadocita()
+  }
+
+  guardar(){
+    this.BD.registroCita(this.id,this.nombre,this.fecha,this.notas,this.carrera,this.motivo)
+    this.limpiar();
+  }
+  limpiar(){
+    this.notas="";this.carrera=""
+    this.motivo="";this.fecha="";
+    this.nombre="";
+    this.registrar=false
+    this.listadocita()
+  }
+  registrarCita(){
+    this.registrar=true
+  }
+  listadocita(){
+    this.BD.listadocita=[]
+    this.hoy= new Date().getFullYear()
+    this.listadoCita=this.BD.listadoCita();
+    this.listadoCita=this.BD.listadocita
   }
 
 }
