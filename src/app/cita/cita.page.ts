d93b003db5ae2f29c6e10efc41f40b29877077e4
Armin-AH <armin.alejandro.ah@gmail.com>
2019-11-23 22:29:50 -0700

    comandos de consola para ver fecha

--- src/app/cita/cita.page.ts
+++ src/app/cita/cita.page.ts
@@ -12,6 +12,7 @@ import { formatDate } from '@angular/common';
   styleUrls: ['./cita.page.scss'],
 })
 export class CitaPage implements OnInit {
+  fechaselec;
   hoy: number;
   notas: string;
   carrera: string;
@@ -20,7 +21,10 @@ export class CitaPage implements OnInit {
   nombre: string;
   id = '';
   listadoCita: JSON[];
+  listadoActual: JSON[];
   registrar = false; 
+  i: number = 0;
+  a;
 
   constructor(private BD:BDService, private alertCtrl: AlertController, @Inject(LOCALE_ID) private locale: string) { }
   //constructor(private BD:BDService) { }
@@ -57,6 +61,16 @@ export class CitaPage implements OnInit {
     this.hoy = new Date().getFullYear();
     this.BD.listadoCita();
     this.listadoCita = this.BD.listadocita;
+    console.log(this.listadoCita);
+
+    
+
+    this.a = this.listadoCita[1]
+    console.log(this.listadoCita[1]);
+
+    /*for(this.i = this.listadoCita.length; this.i++; ){
+      if(this.listadoCita[1].fecha > "2019-11-29T16:00:25.232-07:00" && this.listadoCita.[this.i].fecha <= "2019-11-30T16:00:25.232-07:00")
+    }*/
   }
   //////////////////////////////////////////////CRUD CITAS////////////////////////////////////////////////
 
@@ -88,10 +102,10 @@ export class CitaPage implements OnInit {
   }
 
   onCurrentDateChanged = (ev: Date) => {
-    console.log(ev);
-    console.log(typeof ev)
-
-    
+    //console.log(ev);
+    //console.log(typeof ev)
+    this.fechaselec = ev
+    //console.log(this.fechaselec);
 
   };
 
