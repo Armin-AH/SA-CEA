590d516e6e8449a25fb095785d3b90847c0e10e1
Abel Rodriguez <abelrodriguez.e@gmail.com>
2019-11-23 18:21:04 -0700

    Se limpiaron datos al editar

--- src/app/bitacora/bitacora.page.ts
+++ src/app/bitacora/bitacora.page.ts
@@ -63,6 +63,11 @@ export class bitacoraPage implements OnInit {
     this.Editar = false;
     console.log(this.listadoBitacora);
     this.listadobitacora();
+    this.nombre = '';
+    this.fecha = '';
+    this.matricula = '';
+    this.carrera = '';
+    this.motivo = '';
   }
 
   editobitacora(item) {
