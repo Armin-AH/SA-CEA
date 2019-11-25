bfe72ce1b18626a9e4f1c385a4f96284f5e04c31
Armin-AH <armin.alejandro.ah@gmail.com>
2019-11-24 17:51:24 -0700

    Manda a login al iniciar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'cita', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'login', loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule) },
   { path: 'cita', loadChildren: () => import('./cita/cita.module').then( m => m.CitaPageModule) },
   { path: 'reporte', loadChildren: () => import('./reporte/reporte.module').then( m => m.ReportePageModule) },
-  { path: 'bitacora', loadChildren: () => import('./bitacora/bitacora.module').then( m => m.bitacoraPageModule) },  {
+  { path: 'bitacora', loadChildren: () => import('./bitacora/bitacora.module').then( m => m.bitacoraPageModule) },
+  {
     path: 'usuarios',
     loadChildren: () => import('./usuarios/usuarios.module').then( m => m.UsuariosPageModule)
   },
